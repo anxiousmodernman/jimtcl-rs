817a8ad362319a18c321a99ee54049298a80ef17
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-09-22 11:48:28 -0700

    Document distinct types of Jim_Obj
    We will need to examine each of these to see the shape of the object
    based on its type. For instance, a dict object can have an empty
    bytes field.

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,16 @@ use std::os::raw::*;
 
 use jimtcl_sys::*;
 
-pub struct Interpreter {
-    inner: *mut Jim_Interp,
-}
-
 #[derive(Debug, PartialEq)]
 pub enum JimVal {
     Str(String),
     Number(f64),
+    // Dict
+    // Bool
+}
+/// A wrapper around the Jim Tcl interpeter. Not thread safe.
+pub struct Interpreter {
+    inner: *mut Jim_Interp,
 }
 
 impl Interpreter {
@@ -22,12 +24,15 @@ impl Interpreter {
         unsafe { Jim_RegisterCoreCommands(interp) };
         Interpreter { inner: interp }
     }
+
     pub fn eval(&self, code: &str) {
         let c = CString::new(code).unwrap();
         unsafe {
             Jim_Eval(self.inner, c.as_ptr() as *const c_char);
         }
     }
+
+    pub fn add_command(&self, cmd: Box<Command>) {}
 
     pub fn get_val(&self, name: &str) -> Option<JimVal> {
         // get by name
@@ -39,11 +44,9 @@ impl Interpreter {
             Jim_NewStringObj(self.inner, cname.as_ptr() as *const c_char, length as c_int)
         };
 
-        // Jim_GetVariable:
-        //     returns NULL if var name not found
+        // Jim_GetVariable returns NULL if var name not found.
         let res = unsafe { Jim_GetVariable(self.inner, jim_obj, JIM_NONE as i32) };
         if res.is_null() {
-            println!("returning none now!");
             return None;
         }
         let type_name = unsafe {
@@ -54,16 +57,36 @@ impl Interpreter {
                 .expect("could not access typeName of Jim_Obj")
         };
         // look up type: where are these enumerated?
+        // jim.c - look for `static const Jim_ObjType` declarations
         match type_name {
+            // scriptline
+            // script
+            // command
+            // variable
+            // reference
+            // int
+            // coerced-double
+            // double
+            // list
+            // dict
+            // index
+            // return-code
+            // expression
+            // scanformatstring
+            // get-enum
+            // dict-substitution
+            // interpolated
+            // string
+            // compared-string
+            // source
             "source" => unsafe {
-                let s = (*res).bytes;
-                let ss = CStr::from_ptr(s).to_str().unwrap().to_owned();
+                let b = (*res).bytes;
+                let s = CStr::from_ptr(b).to_str().unwrap().to_owned();
                 Jim_Free(jim_obj as *mut c_void);
-                return Some(JimVal::Str(ss));
+                return Some(JimVal::Str(s));
             },
             _ => unimplemented!("got type: {:?}", type_name),
         }
-        // return appropriate JimVal for type
 
         unsafe { Jim_Free(jim_obj as *mut c_void) };
         None
@@ -71,17 +94,36 @@ impl Interpreter {
 }
 
 pub trait Command {
-    // init
+    fn name(&self) -> &str;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    struct MyCmd;
+    impl Command for MyCmd {
+        fn name(&self) -> &str {
+            "mycmd"
+        }
+    }
     #[test]
     fn simple_get_val() {
         let i = Interpreter::new();
-        let code = "set a 69";
+        let code = "
+        # This is Tcl code
+        set a 69
+        set b 420
+        if 0 { puts hello }
+        proc procName { argument } { 
+            # script contents
+            return
+        }
+        set d [dict create foo baz]
+        ";
         i.eval(code);
         assert_eq!(Some(JimVal::Str("69".to_string())), i.get_val("a"));
+        assert_eq!(Some(JimVal::Str("420".to_string())), i.get_val("b"));
+        assert_eq!(None, i.get_val("c"));
+        assert_eq!(None, i.get_val("d"));
     }
 }
