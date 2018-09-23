a660025819c0a113013b442a7696f1620e74397a
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-09-23 14:53:29 -0700

    just make install for now :(

--- jimtcl-sys/build.rs
+++ jimtcl-sys/build.rs
@@ -7,29 +7,14 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    //if !Path::new("jimtcl/.git").exists() {
-    Command::new("git")
-        .args(&["submodule", "update", "--init", "--recursive"])
-        .status()
-        .unwrap();
-    //}
-    let _ = Command::new("./configure")
-        .args(&[
-            "--with-ext=\"oo tree binary sqlite3\"",
-            "--enable-utf8",
-            "--ipv6",
-            "--disable-docs",
-        ]).current_dir("jimtcl")
-        .output()
-        .expect("configure failed");
-    let _ = Command::new("make")
-        .current_dir("jimtcl")
-        .output()
-        .expect("make failed");
-
+    if !Path::new("jimtcl/.git").exists() {
+        Command::new("git")
+            .args(&["submodule", "update", "--init", "--recursive"])
+            .status()
+            .unwrap();
+    }
     println!("cargo:rustc-link-search=./jimtcl");
     println!("cargo:rustc-link-lib=jim");
-    println!("cargo:include=./jimtcl");
     let bindings = bindgen::Builder::default() // The input header we would like to generate         // bindings for.
         .header("wrapper.h") // Finish the builder and generate the bindings.
         .generate() // Unwrap the Result and panic on failure.
