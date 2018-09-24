e590c29225f7dac5fd232b738429f3f6c9837995
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-09-23 23:12:41 -0700

    do dumb stuff like a cool guy

--- jimtcl-sys/build.sh
+++ jimtcl-sys/build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 (
     cd jimtcl
+    echo "JIM TIME"
     ./configure --with-ext="oo tree binary sqlite3" --enable-utf8 --ipv6 --disable-docs
+    echo "CONFGIURE DONE"
+    ls
+
     git am ../1508-epic-patches.patch
+    echo "PATCH APPLIED:"
+    head jim-subcmd.h
     make
+    echo "make supposedly finished"
 )
